4c7e4fdb7980e66bbffe7e1f817ebaa5e227e046
zhushuaijie <szhu9903@gmail.com>
2023-05-12 17:24:51 +0800

    feat(full103 env):sparkles:: full103设备查询视图更新

--- hardware_tcp_server/db/db_view.sql
+++ hardware_tcp_server/db/db_view.sql
@@ -5,3 +5,14 @@ SELECT
 FROM Hardware_Equip a
 LEFT JOIN Hardware_Type b ON (a.he_type = b.id)
 
+CREATE OR REPLACE VIEW Hardware_Equip_Full103 AS
+SELECT
+    a.*,
+    b.ht_name,
+    c.fe_humidity,c.fe_temperature,
+    d.id AS relay_id,d.fr_controlmode,d.fr_switch
+FROM Hardware_Equip a
+LEFT JOIN Hardware_Type b ON (a.he_type = b.id)
+LEFT JOIN Full103_Env c ON (a.id = c.fe_equipid)
+left join Full103_Relay d ON (a.id = d.fr_equipid)
+where a.he_effect=1 and a.he_type=2
